9f48699f34f959bf383a6c66d94b8c7817f97525
UkYi <ukyi.js@gmail.com>
2020-07-04 13:43:59 +0900

    feat: cat entity 추가

--- src/cats/model/cats.entity.ts
+++ src/cats/model/cats.entity.ts
@@ -5,7 +5,7 @@ import * as uuid from 'uuid';
 
 @Entity()
 @ObjectType()
-export class Cat {
+export class Cats {
   @Expose()
   @ObjectIdColumn()
   @Field()
@@ -26,9 +26,9 @@ export class Cat {
   @Field(() => Date)
   createdAt: number;
 
-  constructor(cat: Partial<Cat>) {
-    if (!cat?.name) return;
-    Object.assign(this, plainToClass(Cat, cat, { excludeExtraneousValues: true }));
+  constructor(cats: Partial<Cats>) {
+    if (!cats?.name) return;
+    Object.assign(this, plainToClass(Cats, cats, { excludeExtraneousValues: true }));
     this._id = this._id ?? uuid.v4();
     this.createdAt = this.createdAt ?? +new Date();
   }
