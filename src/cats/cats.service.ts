3cff2d1da7c3be5842a55b66e08af7be2bc9fe43
UkYi <ukyi.js@gmail.com>
2020-07-04 13:58:38 +0900

    feat: cats service 추가

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cats } from './model/cats.entity';
+import { MongoRepository } from 'typeorm';
+
+@Injectable()
+export class CatsService {
+  constructor(@InjectRepository(Cats) private readonly catsRepository: MongoRepository<Cats>) {}
+
+  findCats(): Promise<Cats[]> {
+    return this.catsRepository.find();
+  }
+
+  findCatsByName(name: string): Promise<Cats> {
+    return this.catsRepository.findOne({ name });
+  }
+}
