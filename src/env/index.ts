ad4478353129a48507bc3b65ed732fa6b202311b
UkYi <ukyi.js@gmail.com>
2020-07-04 13:11:39 +0900

    feat: mongodb env 추가

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,17 @@
 import 'dotenv/config';
 
-export const { PORT = 3000, NODE_ENV = 'production', IS_OFFLINE = false } = process.env;
+export const {
+  PORT = 3000,
+  NODE_ENV = 'production',
+  IS_OFFLINE,
+  MONGODB_PORT = 11049,
+  MONGODB_ATLAS_USER,
+  MONGODB_ATLAS_PASS,
+  MONGODB_ATLAS_HOST,
+  MONGODB_ATLAS_DATABASE,
+} = process.env;
+
 export const IS_PRODUCTION = NODE_ENV === 'production';
+
+export const MONGODB_ATLAS_URL = `mongodb+srv://${MONGODB_ATLAS_USER}:${MONGODB_ATLAS_PASS}${MONGODB_ATLAS_HOST}/${MONGODB_ATLAS_DATABASE}`;
+export const MONGODB_URL = process.env.MONGODB_PORT ? `mongodb://localhost:${MONGODB_PORT}` : MONGODB_ATLAS_URL;
