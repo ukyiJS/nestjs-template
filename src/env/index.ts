510625ffbd591ccd88d715caec35151340a7a798
UkYi <ukyi.js@gmail.com>
2020-07-04 13:01:42 +0900

    feat: IS_OFFLINE 추가

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 
-export const { PORT = 3000, NODE_ENV = 'production' } = process.env;
+export const { PORT = 3000, NODE_ENV = 'production', IS_OFFLINE = false } = process.env;
 export const IS_PRODUCTION = NODE_ENV === 'production';
