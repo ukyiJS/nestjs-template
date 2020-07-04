da3c93c5d578182ade35a6efc72cd9b00bb59e54
UkYi <ukyi.js@gmail.com>
2020-07-04 13:04:29 +0900

    feat: graphql module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
-import { CacheService } from './config';
+import { CacheService, GraphqlService } from './config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [CacheModule.registerAsync({ useClass: CacheService })],
+  imports: [
+    CacheModule.registerAsync({ useClass: CacheService }),
+    GraphQLModule.forRootAsync({ useClass: GraphqlService }),
+  ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
 })
