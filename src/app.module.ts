e47172147b008603f6221b7b5d3950780e7c09c4
UkYi <ukyi.js@gmail.com>
2020-07-04 13:03:30 +0900

    feat: app resolver 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module, CacheModule } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 import { CacheService } from './config';
 
 @Module({
   imports: [CacheModule.registerAsync({ useClass: CacheService })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
