c40f9f43f3016958167c99b76ad4028514dec0ae
UkYi <ukyi.js@gmail.com>
2020-07-04 13:58:15 +0900

    feat: cats module 추가

---
+++ src/cats/cats.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CatsResolver } from './cats.resolver';
+import { CatsService } from './cats.service';
+import { Cats } from './model/cats.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Cats])],
+  providers: [CatsService, CatsResolver],
+})
+export class CatsModule {}
