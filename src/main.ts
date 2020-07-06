3e3b755d1610b1410230e3b27874cfccbcef6a3f
UkYi <ukyi.js@gmail.com>
2020-07-06 23:23:52 +0900

    feat: writeJson 제거

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { AppModule } from '@/app.module';
-import * as env from '@/env';
-import { writeJson } from '@/utils';
+import { PORT } from '@/env';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { LoggingInterceptor, TimeoutInterceptor } from './common';
 
 const createServer = async () => {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor()).listen(env.PORT);
+  await app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor()).listen(PORT);
 
-  Logger.log(`Server running on http://localhost:${env.PORT}`, 'Server');
+  Logger.log(`Server running on http://localhost:${PORT}`, 'Server');
 };
-writeJson({ data: env, fileName: env.IS_PRODUCTION ? 'env.prod' : 'env.dev' });
 createServer();
