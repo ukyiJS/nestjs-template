a037ed91fbbc13a7e839c9984f013851060fdc78
ukyiJS <ukyi.js@gmail.com>
2020-07-06 17:59:44 +0900

    feat: dotenv.config 추가

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,8 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+const path = join(process.cwd(), process.env.NODE_ENV !== 'production' ? '.env.dev' : '.env.prod');
+dotenv.config({ path });
 
 export const {
   PORT = 3000,
