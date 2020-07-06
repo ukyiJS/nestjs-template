53aaf2d60dbdf9868af188695d6db1164389b310
UkYi <ukyi.js@gmail.com>
2020-07-06 23:25:29 +0900

    feat: MODE 추가

--- src/utils/writeJson.ts
+++ src/utils/writeJson.ts
@@ -1,21 +1,23 @@
 import { Logger } from '@nestjs/common';
-import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import * as fs from 'fs';
 import { join } from 'path';
+
+const { mkdir, writeFile } = fs.promises;
 
 type WriteJson = {
   data: any;
   fileName: string;
   dirName?: string;
 };
-export const writeJson = ({ data, fileName, dirName = '' }: WriteJson): void => {
+export const writeJson = async ({ data, fileName, dirName = '' }: WriteJson): Promise<void> => {
   const saveDir = join(process.cwd(), dirName);
+  const jsonFileName = `${saveDir}/${fileName}.json`;
 
-  if (!existsSync(saveDir)) {
-    mkdirSync(saveDir);
-    Logger.log(`Add ${saveDir}`, 'Directory');
+  if (!fs.existsSync(saveDir)) {
+    await mkdir(saveDir);
+    Logger.log(saveDir, 'WriteDir');
   }
 
-  const jsonFileName = `${saveDir}/${fileName}.json`;
-  writeFileSync(jsonFileName, JSON.stringify(data));
-  Logger.log(`Add ${jsonFileName}`, 'Json');
+  await writeFile(jsonFileName, JSON.stringify(data));
+  Logger.log(jsonFileName, 'WriteFile');
 };
