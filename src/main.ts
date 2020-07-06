8409286c6bb8b49004813c8ca95eaa1e03c4110a
ukyiJS <ukyi.js@gmail.com>
2020-07-06 17:59:33 +0900

    feat: writeJson 추가

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { PORT } from '@/env';
+import { AppModule } from '@/app.module';
+import * as env from '@/env';
+import { writeJson } from '@/utils';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { LoggingInterceptor, TimeoutInterceptor } from './common';
 
 const createServer = async () => {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor()).listen(PORT);
+  await app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor()).listen(env.PORT);
 
-  Logger.log(`Server running on http://localhost:${PORT}`, 'Server');
+  Logger.log(`Server running on http://localhost:${env.PORT}`, 'Server');
 };
+writeJson({ data: env, fileName: env.IS_PRODUCTION ? 'env.prod' : 'env.dev' });
 createServer();
