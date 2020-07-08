0f00e7304235e2e4b5f6393a8c29dc24c2f2f6bb
ukyiJS <ukyi.js@gmail.com>
2020-07-08 10:37:57 +0900

    feat: env config path 설정

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,3 +1,7 @@
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+const path = join(process.cwd(), process.env.NODE_ENV !== 'production' ? '.env.dev' : '.env.prod');
+dotenv.config({ path });
 
 export const { PORT = 3000, NODE_ENV = 'production' } = process.env;
